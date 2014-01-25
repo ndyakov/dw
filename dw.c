afe14a2ad439c1cc281ff991c00986c0173de258
Aleksandar Ivanov <aivanov92@gmail.com>
2014-01-25 20:39:23 +0000

    Show help if --help is passed

--- dw.c
+++ dw.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "aircrack-ng/src/osdep/osdep.h"
+
+void print_help()
+{
+    printf("dw <interface>")
+}
 
 int main(int argc, const char *argv[])
 {
     if (geteuid() != 0)
     {
         printf("This program requires root privileges.\n");
+        return 1;
+    }
+
+    if (argc < 2  || !memcmp(argv[1], "--help", 6))
+    {
+        printf("need help");
         return 1;
     }
 
