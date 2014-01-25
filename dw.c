8a6788cceaa5195fb461e158bde90c4e8f5d5c3f
Aleksandar Ivanov <aivanov92@gmail.com>
2014-01-25 21:40:10 +0200

    Check if the user is with root privileges

--- dw.c
+++ dw.c
@@ -1,5 +1,14 @@
+#include <stdio.h>
+
 #include "aircrack-ng/src/osdep/osdep.h"
 
-int main(int argc, const char *argv[]) {
+int main(int argc, const char *argv[])
+{
+    if (geteuid() != 0)
+    {
+        printf("This program requires root privileges.\n");
+        return 1;
+    }
+
     return 0;
 }
